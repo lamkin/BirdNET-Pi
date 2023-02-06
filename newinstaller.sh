be544b420574d5a707cc5da7325c7fefb4a07ef2
David Lamkin <drl@metanate.com>
2023-02-06 16:13:14 +0000

    Install from drl/tmpfile-use branch of drl repository

--- newinstaller.sh
+++ newinstaller.sh
@@ -39,8 +39,8 @@ if [[ ! -z $PACKAGES_MISSING ]] ; then
   sudo apt -y install $PACKAGES_MISSING
 fi
 
-branch=main
-git clone -b $branch --depth=1 https://github.com/mcguirepr89/BirdNET-Pi.git ${HOME}/BirdNET-Pi &&
+branch=drl/tmpfile-use
+git clone -b $branch --depth=1 https://github.com/lamkin/BirdNET-Pi.git ${HOME}/BirdNET-Pi &&
 
 $HOME/BirdNET-Pi/scripts/install_birdnet.sh
 if [ ${PIPESTATUS[0]} -eq 0 ];then
